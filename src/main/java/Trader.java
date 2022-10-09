f6f86a6e658351d5f0c66bff6a24dd2ad207de56
dallasschn <dallas.schnitzius@mail.utoronto.ca>
2022-10-09 17:16:37 -0400

    java activity 4

--- src/main/java/Trader.java
+++ src/main/java/Trader.java
@@ -35,12 +35,19 @@ public class Trader<T> {
         this.money = money;
     }
 
+    public Trader(int money) {
+        this.money = money;
+        this.inventory = new ArrayList<>();
+        this.wishlist = new ArrayList<>();
+    }
     /* TODO: Add a new constructor that takes a single argument
      *       representing the Trader's money. Give the Trader
      *       empty ArrayLists for their inventory and wishlist.
      */
 
-
+    public void addToWishlist(T item) {
+        this.wishlist.add(item);
+    }
 
 
 
@@ -51,6 +58,12 @@ public class Trader<T> {
 
 
 
+    public int getSellingPrice(T item) {
+        if (item instanceof Tradable) {
+            return ((Tradable) item).getPrice();
+        }
+        return Tradable.MISSING_PRICE;
+    }
 
     /* TODO: Implement the method getSellingPrice that takes an
      *       object of type T and returns the object's price
